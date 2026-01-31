176ce80346ef77ab4f23d53e3a84774a14316941
amadzai <20966134+amadzai@users.noreply.github.com>
2026-01-31 17:58:58 +0800

    feat: Setup main with swagger and validations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,38 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { BigIntSerializerInterceptor } from './interceptors/big-int-serializer.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector)),
+    new BigIntSerializerInterceptor(),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('gambAIt API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.BACKEND_PORT ?? 3001, '0.0.0.0');
 }
-bootstrap();
+void bootstrap();
