206678eb2184bc7068fb467fc831bcd200e8083b
amadzai <20966134+amadzai@users.noreply.github.com>
2026-02-04 22:57:44 +0800

    feat: Add agent service module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,11 @@ import { AppService } from './app.service.js';
 import { PrismaModule } from './service-modules/prisma/prisma.module.js';
 import { ChessModule } from './api-modules/chess/chess.module.js';
 import { ChessServiceModule } from './service-modules/chess-service/chess-service.module.js';
+import { AgentsModule } from './api-modules/agents/agents.module.js';
 import { BigIntSerializerInterceptor } from './interceptors/big-int-serializer.interceptor.js';
 
 @Module({
-  imports: [PrismaModule, ChessServiceModule, ChessModule],
+  imports: [PrismaModule, ChessServiceModule, ChessModule, AgentsModule],
   controllers: [AppController],
   providers: [
     AppService,
@@ -33,4 +34,4 @@ import { BigIntSerializerInterceptor } from './interceptors/big-int-serializer.i
     },
   ],
 })
-export class AppModule {}
+export class AppModule { }
